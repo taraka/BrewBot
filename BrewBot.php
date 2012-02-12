11a9b9b48a76e2c568b71c81f3192f92e19f8415
Tom <tom@codebolt.co.uk>
2012-02-12 23:49:31 +0000

    Starting work on brewbot app

--- BrewBot.php
+++ BrewBot.php
@@ -8,13 +8,14 @@ class Application
 {
 	private
 		$_partisipants,
-		$_times,
-		$_lastBrew;
+		$_times = array('23:22'),
+		$_lastBrew,
+		$_currentTime,
+		$_running = true;
 		
 	
 	public function __construct()
 	{
-		
 	}
 	
 	/**
@@ -24,10 +25,68 @@ class Application
 	 */
 	public function run()
 	{
-		while (!System_Daemon::isDying())
+		while (!System_Daemon::isDying() && $this->isRunning())
 		{
-			System_Daemon::info("Write info");
-			System_Daemon::iterate(2);
+			$this->setCurrentTimeMark();
+			
+			if ($this->isTimeForABrew())
+			{
+				System_Daemon::info('Time for a brew');
+				$this->setLastBrewTime();
+				$this->notifyPartisipants();
+			}
+			
+			$this->_running = false; 
+			System_Daemon::iterate(20);
 		}
+	}
+	
+	/**
+	 * Sets up the current time variable for this iteration
+	 * 
+	 * @return void
+	 */
+	private function setCurrentTimeMark()
+	{
+		$this->_currentTime = date('H:m');
+		System_Daemon::info('Updated current time to: ' . $this->_currentTime);
+	}
+	
+	/**
+	 * Returns true if the daemon should tell people its time for a brew
+	 * 
+	 * @return bool
+	 */
+	private function isTimeForABrew()
+	{
+		return false;
+	}
+	
+	/**
+	 * Marks the last brew time so we don't sent notifications twice
+	 * 
+	 * @return void
+	 */
+	private function setLastBrewTime()
+	{
+		$this->_lastBrew = $this->_currentTime;
+	}
+	
+	/**
+	 * Notify the partisipants that its time for a brew
+	 */
+	private function notifyPartisipants()
+	{
+		
+	}
+	
+	/**
+	 * Returns true if the daemon is not exiting
+	 * 
+	 * @return boolean
+	 */
+	public function isRunning()
+	{
+		return (bool)$this->_running;
 	}
 }
