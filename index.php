049d809876a7e1b6155bc35c0ad2669c61934d09
Tom Rawcliffe <tom@labelmedia.co.uk>
2012-02-17 15:41:06 +0000

    Adding some options to the script

--- index.php
+++ index.php
@@ -9,11 +9,41 @@ System_Daemon::setOption("appDescription", "Brewbot sends brew notifications to 
 System_Daemon::setOption("authorEmail", "tom@codebolt.co.uk");
 System_Daemon::setOption("authorName", "Tom Rawcliffe");
 
-System_Daemon::start();
+$runmode = array(
+    'no-daemon' => false,
+    'help' => false,
+    'write-initd' => false,
+);
 
-//System_Daemon::writeAutoRun();
+foreach ($GLOBALS['argv'] as $k=>$arg) 
+{
+	if (substr($arg, 0, 2) == '--' && isset($runmode[substr($arg, 2)])) {
+		$runmode[substr($arg, 2)] = true;
+	}
+}
 
-$app = new \BrewBot\Application();
-$app->run();
+if ($runmode['help']) {
+	echo "no help available\n";
+	exit;
+}
+
+if (!$runmode['no-daemon'] && !$runmode['write-initd']) {
+	System_Daemon::start();
+}
+
+if ($runmode['write-initd'])
+{
+	if (($initd_location = System_Daemon::writeAutoRun()) === false) {
+		System_Daemon::info("unable to write init.d script");
+	}
+	else {
+		System_Daemon::info('sucessfully written startup script: ' . $initd_location . "\n");
+    }
+}
+else
+{
+	$app = new \BrewBot\Application();
+	$app->run();
+}
 
 System_Daemon::stop();
