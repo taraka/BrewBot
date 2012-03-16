9323d4d5dca84a7e98722aa01532f021598fbc91
martin <martin@labelmedia.co.uk>
2012-03-16 13:24:01 +0000

    spelling fix

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once "System/Daemon.php";
 require_once "BrewBot.php";
 
 System_Daemon::setOption("appName", "brewbot");
-System_Daemon::setOption("appDescription", "Brewbot sends brew notifications to partisipants");
+System_Daemon::setOption("appDescription", "Brewbot sends brew notifications to participants");
 System_Daemon::setOption("authorEmail", "tom@codebolt.co.uk");
 System_Daemon::setOption("authorName", "Tom Rawcliffe");
 
