1ee28161b292d58f2a95d4bd1c9c57728ce87915
martin <martin@labelmedia.co.uk>
2012-03-27 13:33:14 +0100

    updated input pointer

--- db/mpm/config/db_config.php
+++ db/mpm/config/db_config.php
@@ -6,7 +6,7 @@ $db_config->port = '3306';
 $db_config->user = 'BrewBot';
 $db_config->pass = 'r6fvfpzd7mH8a';
 $db_config->name = 'BrewBot';
-$db_config->db_path = '/Users/tom/Sites/BrewBot/db/mpm/db/';
+$db_config->db_path = '/Users/martin/Sites/BrewBot/db/mpm/db/';
 $db_config->method = 1;
 $db_config->migrations_table = 'mpm_migrations';
 
