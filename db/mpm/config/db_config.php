bd617986750a7faedd8d83a027b46c54597e765f
Tom Rawcliffe <tom@labelmedia.co.uk>
2012-03-27 09:10:06 +0100

    Adding mpm for db patches

---
+++ db/mpm/config/db_config.php
@@ -0,0 +1,13 @@
+<?php
+
+$db_config = (object) array();
+$db_config->host = 'localhost';
+$db_config->port = '3306';
+$db_config->user = 'BrewBot';
+$db_config->pass = 'r6fvfpzd7mH8a';
+$db_config->name = 'BrewBot';
+$db_config->db_path = '/Users/tom/Sites/BrewBot/db/mpm/db/';
+$db_config->method = 1;
+$db_config->migrations_table = 'mpm_migrations';
+
+?>
